3100317abd85dd95bbdb4452bd710a7dc634dfc4
Charinda Moyer <cmoyer2@spscc.edu>
2022-11-03 11:02:32 -0700

    Add project files.

---
+++ app/src/main/java/androidbootcamp/net/NorthThurstonHigh.java
@@ -0,0 +1,14 @@
+package androidbootcamp.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NorthThurstonHigh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_north_thurston_high);
+    }
+}
---
+++ app/src/main/java/androidbootcamp/net/OlympiaHigh.java
@@ -0,0 +1,14 @@
+package androidbootcamp.net;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OlympiaHigh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olympia_high);
+    }
+}
